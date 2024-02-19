2f6ef61dd581d2e2d087c085a888da4ed4ffd5ef
Prashant Chakraborty <prashantchakraborty@Prashants-MBP.attlocal.net>
2024-02-18 20:57:25 -0600

    Namaste React episode 2

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement(
     'div', 
     { id: 'parent' }, 
     React.createElement('div', { id: 'child' }, 
     [
-        React.createElement('h1', {}, 'h1 tag here'),
-        React.createElement('h2', {}, 'h2 tag here')
+        React.createElement('h1', {}, 'This is Namaste React 🚀'),
+        React.createElement('h2', {}, 'Prashant Chakraborty')
     ]),
     React.createElement('div', { id: 'child'}, 
     [
@@ -14,8 +16,7 @@ const parent = React.createElement(
     ]),
 );
 
-
-console.log(parent);
+console.log(parent); // object
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
